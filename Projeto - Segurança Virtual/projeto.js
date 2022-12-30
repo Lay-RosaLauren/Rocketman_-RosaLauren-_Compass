26f72f731265680f77d0718cb33acc54cb4fdf97
Lay-RosaLauren <charmehaut@uol.com.br>
2022-12-30 11:41:45 -0300

    Projeto - Segurança Virtual

--- Projeto - Segurança Virtual/projeto.js
+++
@@ -1,9 +0,0 @@
-function VerificarEntrada() {
-    NomeConvidado = document.getElementById('nome').value;
-    ConvidadosLay = ['Lay', 'Marina', 'Nathy', 'Neto', 'Renato', 'Alfredo', 'Zanon'];
-    if (ConvidadosLay.includes(NomeConvidado)) {
-        document.getElementById('PermissaoDeEntrada').innerText = 'Você pode Entrar!';
-    } else {
-        document.getElementById('PermissaoDeEntrada').innerText = 'Você não pode Entrar!';
-    }
-}
